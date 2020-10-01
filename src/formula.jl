3252406f1e35f8ca311448d11837e05025127720
Phillip Alday <me@phillipalday.com>
2020-10-02 01:58:56 +0200

    fix fulldummy

--- src/formula.jl
+++ src/formula.jl
@@ -4,20 +4,48 @@ function convert_julia_to_r(f::StatsModels.FormulaTerm)::AbstractString
     for (idx, trm) in enumerate(f.rhs)
 
         if trm isa MixedModels.ZeroCorr
-            for tt in trm.term.lhs.terms
+            grp = trm.term.rhs.sym
+            terms = Vector{String}(undef, length(trm.term.lhs.terms))
+            
+            
+            for (idx,tt) in enumerate(trm.term.lhs.terms)
                 if tt isa CategoricalTerm 
                     size(tt.contrasts.matrix, 2) == 2 || 
                         throw(ArgumentError("zerocorr for factors with more than 2 levels not supported")) 
-                end 
+                end
+                
+                if tt isa InterceptTerm
+                    terms[idx] = "( 1 | $grp)"
+                elseif tt.contrasts isa StatsModels.ContrastsMatrix{StatsModels.FullDummyCoding}
+                    terms[idx] = "( 0 + dummy($(tt.sym), levels($(tt.sym))) | $grp)"
+                else
+                    terms[idx] = "( 0+ $(tt.sym) | $grp)"
+                end
+                     
             end
         
-            strtrm = replace(string(trm), "MixedModels.ZeroCorr((" =>"")
-            offset = match(r"\|.*\)\)", strtrm).offset
-            grp = strip(strtrm[offset+1:end-2])
-            pred = strip(strtrm[begin:offset-1])
-            septerms = ("""($(tt == "1" ? tt : "0 + $tt") | $grp)""" for tt in strip.(split(pred, r"[+*]")))
+            # strtrm = replace(string(trm), "MixedModels.ZeroCorr((" =>"")
+            # offset = match(r"\|.*\)\)", strtrm).offset
+            # grp = strip(strtrm[offset+1:end-2])
+            # pred = strip(strtrm[begin:offset-1])
+            # septerms = ("""($(tt == "1" ? tt : "0 + $tt") | $grp)""" for tt in strip.(split(pred, r"[+*]")))
             
-            rhs[idx] = foldl((x,y) -> "$x + $y", septerms)    
+            rhs[idx] = foldl((x,y) -> "$x + $y", terms)    
+        elseif trm isa MixedModels.RandomEffectsTerm
+            grp = trm.rhs.sym
+            terms = Vector{String}(undef, length(trm.lhs.terms))
+            
+            for (idx,tt) in enumerate(trm.lhs.terms)   
+                if tt isa CategoricalTerm && 
+                    tt.contrasts isa StatsModels.ContrastsMatrix{StatsModels.FullDummyCoding}
+                    terms[idx] = "dummy($(tt.sym), levels($(tt.sym)))"
+                else
+                    terms[idx] = string(tt)
+                end
+                     
+            end
+        
+            rhs[idx] = "( " * foldl((x,y) -> "$x + $y", terms) * " | $grp)"
         else
             rhs[idx] = string(trm)    
         end
