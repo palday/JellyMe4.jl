76592bf1550e51b7ceb9f0f34cfc20678d4f25ef
Phillip Alday <palday@users.noreply.github.com>
2022-09-13 14:59:07 +0000

    change most lme4 sanity check errors to warnings (#59)
    * change most lme4 sanity checks to warnings
    
    * patch bump
    
    * oops
    
    * ci fix

--- src/lmerMod.jl
+++ src/lmerMod.jl
@@ -110,11 +110,9 @@ function sexp(::Type{RClass{:lmerMod}}, x::Tuple{LinearMixedModel{T}, DataFrame}
     jellyme4_mod <- $LMER(formula = $(formula),
                            data=jellyme4_data,
                            REML=$(REML),
-                           control=lme4::lmerControl(optimizer="nloptwrap",
-                                                     optCtrl=list(maxeval=$(rsteps)),
-                                    calc.derivs=FALSE,
-                                    check.nobs.vs.nRE= "warning"),
-                            start=list(theta=jellyme4_theta))
+                           control=lme4::lmerControl(optCtrl=list(maxeval=$(rsteps)),
+                                                     $(join(MERCONTROL_OPTIONS,","))),
+                           start=list(theta=jellyme4_theta))
      jellyme4_mod@optinfo\$feval <- $(feval)
      jellyme4_mod@optinfo\$message <- "$(message)"
      jellyme4_mod@optinfo\$optimizer <- "$(optimizer)"
