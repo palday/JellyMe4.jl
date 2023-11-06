78a51a362634018c5511edbc22e3c439076f494c
Phillip Alday <palday@users.noreply.github.com>
2023-11-06 23:30:23 +0000

    spellcheck (#69)
    * spellcheck CI
    
    * tpos

--- src/merMod.jl
+++ src/merMod.jl
@@ -20,7 +20,7 @@ end
 sexpclass(x::MixedModel{T}) where {T} = RClass{:merMod}
 
 # we could in theory support the other ordering by re-ordering and passing on
-# but that introduces additional maintainance work and encourages messy style
+# but that introduces additional maintenance work and encourages messy style
 function sexp(::Type{RClass{:merMod}}, x::Tuple{Union{DataFrame,ColumnTable},MixedModel})
     throw(ArgumentError("The order in your Tuple is reversed. It should be (Model, Data)"))
 end
